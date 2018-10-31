589e8ecf312169941d828b55dd9669843b80aeb8
CodeYoga80s <CodeYoga80s@gmail.com>
2018-10-30 22:51:00 -0400

    pushing up final updates to git lab

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,27 +1,27 @@
 var friends = [
 	{
 		name: "Gordon Gekko",
-		photo: "https://placehold.it/200x200",
+		photo: "http://images4.fanpop.com/image/photos/22300000/Michael-Douglas-michael-douglas-22334430-316-400.jpg",
 		scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1]
 	},
 	{
 		name: "Patrick Bateman",
-		photo: "https://placehold.it/200x200",
+		photo: "http://images2.fanpop.com/images/photos/7600000/Patrick-Bateman-american-psycho-7627157-400-289.jpg",
 		scores: [2, 5, 4, 1, 2, 3, 3, 2, 3, 3]
 	},
 	{
 		name: "Jordan Belfort",
-		photo: "https://placehold.it/200x200",
+		photo: "http://images6.fanpop.com/image/photos/36600000/the-wolf-of-wall-street-image-the-wolf-of-wall-street-36654975-480-360.jpg",
 		scores: [1, 1, 5, 3, 3, 5, 1, 2, 3, 4]
 	},
 	{
 		name: "Bud Fox",
-		photo: "https://placehold.it/200x200",
+		photo: "http://images6.fanpop.com/image/photos/32100000/Charlie-Sheen-charlie-sheen-32127907-544-640.jpg",
 		scores: [3, 1, 3, 5, 2, 3, 2, 2, 5, 5]
 	},
 	{
 		name: "Jim Young",
-		photo: "https://placehold.it/200x200",
+		photo: "http://images2.fanpop.com/images/photos/5000000/Ben-Affleck-ben-affleck-5068670-300-400.jpg",
 		scores: [3, 1, 4, 4, 2, 4, 2, 5, 1, 3]
 	}
 ];
